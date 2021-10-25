811f2128cb2c5f0a8fabb50ef3bc12435052138b
Alessandra Vargas-Alfaro <10880639@uvu.edu>
2021-10-25 11:26:03 -0600

    Added Pooled Objects, Enemy and Nav Mesh

--- Prototype 3 - First Person Game/Assets/Scripts/Weapon.cs
+++ Prototype 3 - First Person Game/Assets/Scripts/Weapon.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    public GameObject bulletPrefab;
+    //public GameObject bulletPrefab; (old code)
+    public ObjectPool bulletPool;   // <--- (new code)
     public Transform muzzle;
 
     public int curAmmo;
@@ -23,7 +24,7 @@ public class Weapon : MonoBehaviour
     {
         // Disable cursor
         Cursor.lockState = CursorLockMode.Locked;
-        
+
         if(GetComponent<PlayerController>())
             isPlayer = true;
     }
@@ -46,7 +47,11 @@ public class Weapon : MonoBehaviour
         lastShootTime = Time.time;
         curAmmo--;
         // Creating an instance of the bullet prefab at muzzles position and rotation
-        GameObject bullet = Instantiate(bulletPrefab, muzzle.position, muzzle.rotation);
+        //GameObject bullet = Instantiate(bulletPrefab, muzzle.position, muzzle.rotation); (old code)
+        GameObject bullet = bulletPool.GetObject(); // <--- (new code)
+        bullet.transform.position = muzzle.position;
+        bullet.transform.rotation = muzzle.rotation;
+
         //add velocity to projectile
         bullet.GetComponent<Rigidbody>().velocity = muzzle.forward * bulletSpeed;
 
