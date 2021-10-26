9a10ec22179fbc2ead886835d57b101cab1ec62d
Alessandra Vargas-Alfaro <10880639@uvu.edu>
2021-10-25 21:47:19 -0600

    Removed any excess coding

--- Prototype 3 - First Person Game/Assets/Scripts/Weapon.cs
+++ Prototype 3 - First Person Game/Assets/Scripts/Weapon.cs
@@ -56,16 +56,4 @@ public class Weapon : MonoBehaviour
         bullet.GetComponent<Rigidbody>().velocity = muzzle.forward * bulletSpeed;
 
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
